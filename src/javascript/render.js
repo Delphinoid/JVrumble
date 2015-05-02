607bdc2a1ae42ad9bbdb3c32809efe86e564d832
Delphinoid <penganddolph@gmail.com>
2015-05-03 09:56:20 +1000

    Replaced setInterval() with requestAnimationFrame() for rendering

--- src/javascript/render.js
+++ src/javascript/render.js
@@ -1,7 +1,9 @@
 function render(){
 	
+	/* Clear the canvas before drawing so images don't leave trails on a transparent background */
 	canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-	
 	guySpr.render(testPlayer.x, testPlayer.y, guySpr.frameWidth, guySpr.frameHeight);
+	
+	requestAnimationFrame(function(){render();});
 	
 }
